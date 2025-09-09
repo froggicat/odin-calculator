a76d7df4ad18b1a81dfe860c831188930e7d1764
froggicat <166259685+froggicat@users.noreply.github.com>
2025-09-09 23:20:16 +0100

    added operate function to script, made calc's body, screen and operator buttons (not yet functional)

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return result = a / b;
 }
+
+let num1 = 0;
+let operator = "";
+let num2 = 0;
+
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        return newNum = num1 + num2;
+    } else if (operator == "-") {
+        return newNum = num1 - num2;
+    } else if (operator == "*") {
+        return newNum = num1 * num2;
+    } else if (operator == "/") {
+        return newNum = num1 / num2;
+    }
+}
